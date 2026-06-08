3295ab1b29a59420c6d8fbfeddacbcc0b8148017
manuelortizortiz <manuel.ortiz.ortiz@gmail.com>
2026-06-07 23:29:16 -0400

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,1 +1,9 @@
+# extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_apscheduler import APScheduler
 
+# Inicializamos las extensiones sin vincularlas a la app todavía
+db = SQLAlchemy()
+login_manager = LoginManager()
+scheduler = APScheduler()
