92dc0445d911b45a522e0813c8ff0815c8605d82
manuelortizortiz <manuel.ortiz.ortiz@gmail.com>
2026-06-08 17:23:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ import os
 from flask import Flask
 from sqlalchemy import text
 from config import Config
-from extensions import db, login_manager, scheduler, migrate
+
+# AQUÍ ESTÁ LA CORRECCIÓN: Agregamos 'mail' al final de esta línea
+from extensions import db, login_manager, scheduler, migrate, mail
 from models.user import User
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Inicializar herramientas
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
-    mail.init_app(app)
+    mail.init_app(app)  # <-- Ahora Python sí sabe qué es 'mail'
+
     login_manager.login_view = 'auth.login'
     login_manager.login_message_category = 'danger'
 
@@ -24,7 +28,6 @@ def create_app():
         db.create_all()
         
         # --- AUTO-MIGRACIÓN DE CAMPOS ---
-        # Esto añade las columnas si no existen, sin romper nada
         try:
             db.session.execute(text("ALTER TABLE orden_trabajo ADD COLUMN IF NOT EXISTS sistema_falla VARCHAR(100)"))
             db.session.execute(text("ALTER TABLE orden_trabajo ADD COLUMN IF NOT EXISTS causa_raiz TEXT"))
