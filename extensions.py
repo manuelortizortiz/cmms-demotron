224abb374583f5955a11c83944bd0003ec91cdf5
manuelortizortiz <manuel.ortiz.ortiz@gmail.com>
2026-06-09 15:56:53 -0400

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,11 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from apscheduler.schedulers.background import BackgroundScheduler
+from flask_apscheduler import APScheduler
 from flask_migrate import Migrate
-from flask_mail import Mail  # <-- NUEVO
+from flask_mail import Mail
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-scheduler = BackgroundScheduler()
+scheduler = APScheduler()
 migrate = Migrate()
-mail = Mail()  # <-- NUEVO
+mail = Mail()
