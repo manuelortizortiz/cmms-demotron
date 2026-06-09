68a8e2c6992203dc974593a33451e901e31ccfe5
manuelortizortiz <manuel.ortiz.ortiz@gmail.com>
2026-06-09 17:56:06 -0400

    Update personal.py

--- models/personal.py
+++ models/personal.py
@@ -1,12 +1,11 @@
-# models/personal.py
 from extensions import db
 from datetime import datetime
 
 class Personal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100))
-    cargo = db.Column(db.String(100), default="Operador")
-    estado = db.Column(db.String(50), default="Activo")
+    cargo = db.Column(db.String(50))
+    estado = db.Column(db.String(20), default="Activo")
     equipo_asignado = db.Column(db.String(50), default="Ninguno")
 
 class RegistroUsoEquipo(db.Model):
@@ -14,10 +13,11 @@ class RegistroUsoEquipo(db.Model):
     fecha = db.Column(db.DateTime, default=datetime.now)
     operador = db.Column(db.String(100))
     codigo_equipo = db.Column(db.String(50))
-    observacion = db.Column(db.Text) 
+    observacion = db.Column(db.Text)
 
 class Mecanico(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    rut = db.Column(db.String(20), nullable=True)  # <-- NUEVO CAMPO RUT
     nombre = db.Column(db.String(100))
-    especialidad = db.Column(db.String(100), default="General")
-    estado = db.Column(db.String(50), default="Activo")
+    especialidad = db.Column(db.String(50))
+    estado = db.Column(db.String(20), default="Activo")
